6acf51067156b17a8763e35dfa6d186dd7a885b9
Joe McGuckin <joemcguckin14@gmail.com>
2020-08-12 12:36:05 -0400

    Added redux

---
+++ src/store/store.ts
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
+
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default function configureStore() {
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+}
